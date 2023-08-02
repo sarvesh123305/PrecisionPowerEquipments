1daf40571d161743467c1aa59ed3444527e29a55
sarvesh123305 <sarvesh123305@gmail.com>
2023-08-02 18:59:13 +0530

    Products CRUD done

--- src/Admin/AddProducts.js
+++ src/Admin/AddProducts.js
@@ -3,10 +3,22 @@ import { storage, db } from "../Database/Firebase1";
 import { getDownloadURL, ref, uploadBytesResumable } from "firebase/storage";
 import { addDoc, collection, serverTimestamp } from "firebase/firestore";
 import Layout from "../components/Layout";
-import { Box, Button, Grid, TextField, Typography } from "@mui/material";
+import {
+  Avatar,
+  Box,
+  Button,
+  FormControl,
+  Grid,
+  InputLabel,
+  MenuItem,
+  Paper,
+  Select,
+  TextField,
+  Typography,
+} from "@mui/material";
 import ProductsGrid from "../Admin/pages/ProductsGrid";
 import { toast } from "react-toastify";
-import { Label } from "@mui/icons-material";
+import { Description, Label } from "@mui/icons-material";
 
 const initialState = {
   modelName: "",
@@ -25,19 +37,26 @@ function AddProducts() {
   const [progress, setprogress] = useState(null);
   const [isSubmit, setisSubmit] = useState(null);
   const [errors, seterror] = useState(null);
+  const [isSolar, setIsSolar] = useState(false);
+  const [selectedFile, setSelectedFile] = useState(null);
+
   if (isSubmit && errors) {
   } // To be modified later
 
   useEffect(() => {
     const uploadFile = () => {
-      // const name = new Date().getTime + file.name;
-      const storageref = ref(storage, "Images/" + file.name);
-      const uploadTask = uploadBytesResumable(storageref, file);
+      const name = new Date().getTime() + file.name;
+
+      console.log(name);
+      const storageRef = ref(storage, "Products/" + category + "/" + file.name);
+      const uploadTask = uploadBytesResumable(storageRef, file);
+
       uploadTask.on(
-        "state changed",
+        "state_changed",
         (snapshot) => {
           const progress =
             (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+          console.log("Upload is " + progress + "% done");
           setprogress(progress);
           switch (snapshot.state) {
             case "paused":
@@ -46,7 +65,6 @@ function AddProducts() {
             case "running":
               console.log("Upload is running");
               break;
-
             default:
               break;
           }
@@ -61,8 +79,9 @@ function AddProducts() {
         }
       );
     };
+    // URL.createObjectURL(selectedFile)
     file && uploadFile();
-  }, [file]);
+  }, [file, category]);
 
   const handleChange = (e) => {
     setData({ ...data, [e.target.name]: e.target.value });
@@ -91,7 +110,7 @@ function AddProducts() {
 
     setisSubmit(true);
     try {
-      await addDoc(collection(db, "users"), {
+      await addDoc(collection(db, "products"), {
         ...data,
         timestamp: serverTimestamp(),
       });
@@ -101,141 +120,153 @@ function AddProducts() {
     }
     // naviga
   };
+  const handleSelect = (e) => {
+
+    console.log("Solar ", e.target.value);
+    if (e.target.value === "Solar") {
+      setIsSolar(true);
+    } else {
+      setIsSolar(false);
+    }
+    setData({ ...data, [e.target.name]: e.target.value });
+  };
+  const handleClear = () => {
+    setData(initialState);
+  };
   return (
     <Layout>
-      <div>
-        <form onSubmit={handleSubmit}>
-          <label>Enter name </label>
-          <input
-            type="text"
-            name="name"
-            // errors={errors.name ? { content:errors.name} : null}
-            // value={name}
-            onChange={handleChange}
-          />
-          <br></br>
+  
+      <Paper
+        sx={{
+          textAlign: "center",
+          margin: "5%",
+          padding: "10px",
+          maxWidth: "90%",
+          width: "100%",
+        }}
+      >
+        <Typography variant="h4" sx={{ marginBottom: "10px" }}>
+          Add Products
+        </Typography>
+        <Box>
+          <Grid container spacing={2} sx={{ justifyContent: "center" }}>
+            {/* First Row */}
+            <Grid item xs={6}>
+              <TextField
+                name="modelName"
+                label="Model Name"
+                placeholder="Enter Model Name"
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+                value={modelName}
+                onChange={handleChange}
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="brand"
+                label="Brand"
+                placeholder="Enter Brand Name"
+                value={brand}
+                onChange={handleChange}
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+              />
+            </Grid>
 
-          <label>Enter Email </label>
-
-          <input
-            type="email"
-            name="email"
-            // value={email}
-            // errors={errors.email ? { content:errors.email} : null}
-            onChange={handleChange}
-          />
-          <br></br>
-          <label>Enter info </label>
-          <input
-            type="text"
-            name="info"
-            // errors={errors.info ? { content:errors.info} : null}
-            value={info}
-            onChange={handleChange}
-          />
-          <br></br>
-
-          <label>Enter contact </label>
-
-          <input
-            type="text"
-            name="contact"
-            // errors={errors.contact ? { content:errors.contact} : null}
-            // value={contact}
-            onChange={handleChange}
-          />
-          <br></br>
-
-          <label>Enter file </label>
-
-          <input
-            type="file"
-            onChange={(event) => {
-              setFile(event.target.files[0]);
-            }}
-          />
-          <button type="submit" disabled={progress !== null && progress < 100}>
-            {" "}
-            submit
-          </button>
-        </form>
-      </div>
-      <Box>
-        <Grid container spacing={2} sx={{ justifyContent: 'center' }}>
-          {/* First Row */}
-          <Grid item xs={6}>
-            <TextField
-              label="Model Name"
-              placeholder="Enter Model Name"
-              InputLabelProps={{ shrink: true }}
-              fullWidth
-              // onChange={(e) => setProductName(e.target.value)}
-              // value={productName}
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Brand"
-              placeholder="Enter Brand Name"
-              InputLabelProps={{ shrink: true }}
-              // value={price}
-              // onChange={(e) => setPrice(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={6}>
-            <TextField
-              label="Manufacturer"
-              placeholder="Enter Manufacturer Name"
-              InputLabelProps={{ shrink: true }}
-              // value={price}
-              // onChange={(e) => setPrice(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <TextField
-              label="Category"
-              placeholder="Enter Category"
-              InputLabelProps={{ shrink: true }}
-              fullWidth
-              // onChange={(e) => setProductName(e.target.value)}
-              // value={productName}
-            />
-          </Grid>
-          {/* Second Row */}
-          <Grid item xs={12}>
-            <TextField
-              label="Description"
-              InputLabelProps={{ shrink: true }}
-              placeholder="Enter Description"
-              // value={description}
-              // onChange={(e) => setDescription(e.target.value)}
-              fullWidth
-            />
-          </Grid>
-
-          <Grid item xs={6}>
-            <TextField
-              label="File"
-              type="file"
-              InputLabelProps={{ shrink: true }}
-              fullWidth
-            />
-          </Grid>
-          <Grid item xs={6}>
-            <Typography sx={{ margin: "10px" }} variant="h6">
-              File selected
-            </Typography>
-          </Grid>
+            <Grid item xs={6}>
+              <TextField
+                name="manufacturer"
+                label="Manufacturer"
+                placeholder="Enter Manufacturer Name"
+                InputLabelProps={{ shrink: true }}
+                value={manufacturer}
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+              <FormControl fullWidth>
+                <InputLabel>Select Category</InputLabel>
+                <Select
+                  name="category"
+                  placeholder="Enter category"
+                  labelId="category"
+                  id="category"
+                  label="category"
+                  value={category}
+                  onChange={handleSelect}
+                >
+                  <MenuItem value={10}>Inverter</MenuItem>
+                  <MenuItem value={20}>Battery</MenuItem>
+                  <MenuItem value={30}>Solar</MenuItem>
+                </Select>
+              </FormControl>
+            </Grid>
+            {isSolar === true && (
+              <Grid item xs={12}>
+                Test
+              </Grid>
+            )}
+            {/* Second Row */}
             <Grid item xs={12}>
-            <Button>Add Product</Button>
-          </Grid>
-        </Grid>
+              <TextField
+                label="Description"
+                InputLabelProps={{ shrink: true }}
+                placeholder="Enter Description"
+                value={info}
+                name="info"
+                onChange={handleChange}
+                fullWidth
+              />
+            </Grid>
 
-        <ProductsGrid />
-      </Box>
+            <Grid item xs={6}>
+              <TextField
+                label="File"
+                type="file"
+                name="file"
+                onChange={(e) => {
+                  setFile(e.target.files[0]);
+                  const file = e.target.files[0];
+                  if (file) {
+                    setSelectedFile(file); // Set the selected file to the state variable
+                  }
+                  // setFileNameForAvatar(e.target.files[0]?.name || "");
+                  console.log("File name " +e.target.files[0]?.name);
+                }}
+                InputLabelProps={{ shrink: true }}
+                fullWidth
+              />
+            </Grid>
+            <Grid item xs={6}>
+            <a href={selectedFile && URL.createObjectURL(selectedFile)}  target="_blank" rel="noreferrer noopener" >
+              <Avatar alt="Remy Sharp" sx={{ width: 100, height: 50 }}  src={selectedFile && URL.createObjectURL(selectedFile)} />
+            </a>
+              </Grid>
+            <Grid item xs={12}>
+              <Button
+                variant="contained"
+                disabled={progress >= 0 && progress < 100}
+                sx={{ margin: "10px" }}
+                color="success"
+                onClick={handleSubmit}
+              >
+                {" "}
+                Add Product
+              </Button>
+              <Button
+                variant="contained"
+                onClick={handleClear}
+                style={{ backgroundColor: "#f0f0f0", color: "#000" }}
+              >
+                Clear
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Paper>
+      <ProductsGrid />
     </Layout>
   );
 }
